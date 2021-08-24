8f8192d6af4febad74f929880e36b0deb960d014
Tito-Rollis <ouvalres@gmail.com>
2021-08-24 15:07:06 +0700

    adding example animation bulletNav

--- components/bulletNav/bulletNav.js
+++ components/bulletNav/bulletNav.js
@@ -1,14 +1,22 @@
 export default function bulletNav(props) {
     let bulletInner = (
-        <div className="rounded-full h-2 w-2 my-auto bg-majorColor hidden"></div>
+        <div className="rounded-full h-1 w-1 my-auto bg-majorColor"></div>
     );
     let bulletOuter = (
         <div className="rounded-full h-2 w-2 bg-gray-900 flex justify-center content-center transition-all">
+            {/* {bulletInner} */}
+        </div>
+    );
+    let bullet = (
+        <div className="rounded-full h-3 w-3 bg-gray-900 flex justify-center content-center transition-all">
             {bulletInner}
         </div>
     );
     return (
-        <div className={`${props.layout} h-14 flex flex-col justify-between`}>
+        <div
+            className={`${props.layout} h-16 flex flex-col items-center justify-between`}
+        >
+            {bullet}
             {bulletOuter}
             {bulletOuter}
             {bulletOuter}
