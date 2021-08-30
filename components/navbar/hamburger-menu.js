d50dee533aac07026c3bdddf3d3e2da5532feed8
Tito-Rollis <ouvalres@gmail.com>
2021-08-30 12:00:11 +0700

    hamburger animation done

--- components/navbar/hamburger-menu.js
+++ components/navbar/hamburger-menu.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 export default function HamburgerMenu() {
     const [clicked, setClicked] = useState(false);
+    const [midDuration, setMidDuration] = useState('duration-1000');
     const [width, setWidth] = useState('w-1');
     const [hidden, setHidden] = useState('');
     const [topWidth, setTopWidth] = useState('w-2');
@@ -19,32 +20,31 @@ export default function HamburgerMenu() {
         document.onload = setTopWidth('w-6');
         document.onload = setBottomWidth('w-6');
         if (clicked) {
-            setHidden('hidden');
-            setTopTransform('rotate-45');
-            setBottomTransform('-rotate-45 -translate-y-4');
+            setMidDuration('duration-300');
+            setHidden('-translate-x-6');
+            setTopTransform('rotate-45 translate-y-2');
+            setBottomTransform('-rotate-45 -translate-y-2');
+        }
+        if (!clicked) {
+            setHidden('');
+            setTopTransform('');
+            setBottomTransform('');
         }
     });
-
-    // if (!clicked) {
-    //     setHidden('');
-    //     setJustify('justify-between');
-    //     setTopTransform('');
-    //     setBottomTransform('');
-    // }
 
     return (
         <button
             onClick={clickHandler}
-            className={`box-border flex flex-col justify-between h-5`}
+            className={`box-border flex flex-col justify-between h-5 overflow-hidden`}
         >
             <span
-                className={`h-1 ${topWidth} ${topTransform} bg-gray-900 rounded transition-all duration-1000`}
+                className={`h-1 ${topWidth} ${topTransform} bg-gray-900 rounded transition-all ${midDuration}`}
             ></span>
             <span
-                className={`h-1 ${width} ${hidden} bg-gray-900 rounded transition-all duration-1000`}
+                className={`h-1 ${width} ${hidden}  bg-gray-900 rounded transition-all ${midDuration}`}
             ></span>
             <span
-                className={`h-1 ${bottomWidth} ${bottomTransform}  bg-gray-900 rounded transition-all duration-1000`}
+                className={`h-1 ${bottomWidth} ${bottomTransform}  bg-gray-900 rounded transition-all ${midDuration}`}
             ></span>
         </button>
     );
